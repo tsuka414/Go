3e2b13a193209293a4a312d1800a736642d79e14
tsuka414 <keita04141537@icloud.com>
2021-10-24 17:26:15 +0900

    if

--- if.go
+++ if.go
@@ -5,6 +5,13 @@ import (
 	"math"
 )
 
-func sqrt(x float64) string { 
-	
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	fmt.Println(sqrt(2), sqrt(-4))
 }
