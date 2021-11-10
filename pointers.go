2c7b2b78a38e8c04d40df2a317ce2049b06c31ed
tsuka414 <keita04141537@icloud.com>
2021-11-10 21:45:39 +0900

    switch

---
+++ pointers.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i
+}
--- switch.go
+++ switch.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"runtime"
 )
 
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away")
+	fmt.Println("Go runs on")
+	switch od := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+    default:
+		// freebsd. openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
 	}
 }
